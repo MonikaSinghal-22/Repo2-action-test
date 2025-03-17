0a9e413b24cfb5815d59957402f2ccd63a5640b2
MonikaSinghal-22 <monikassinghal1996@gmail.com>
2025-03-17 18:00:51 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "🚀 Running Validation in Docker Action..."
 
 # Save input-data to a file
-echo "$INPUT_JSON" > formatted_data.json
+#echo "$INPUT_JSON" > formatted_data.json
 
 # Run validation script
 python /app/validate.py
