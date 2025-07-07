d0c03b5a0d81e62c9e17c25eed7b7d64b3cec2dd
MonikaSinghal-22 <monikassinghal1996@gmail.com>
2025-07-07 17:20:35 -0400

    python file updated to see values

--- scripts/v_config_processor.py
+++ scripts/v_config_processor.py
@@ -1,1 +1,20 @@
-print("hello")
+import argparse
+import yaml
+
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add('--config', default="config/v_config.yaml")
+    args = parser.parse_args()
+    
+    v_config = load_yaml(args.config)
+    
+    print(f"loaded from '{args.config}'")
+    
+    print(v_config)
+
+if __name__ == "__main__":
+    main()
